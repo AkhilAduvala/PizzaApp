85e4aae88d681630111c003dba1a257b6421e207
AKHIL ADUVALA <aduvalaakhil@gmail.com>
2025-04-20 09:03:55 +0530

    js to ts conversion

---
+++ index.ts
@@ -0,0 +1,63 @@
+const menu = [
+    { name : "Margherita", price : 200},
+    { name : "Pepperoni", price : 250},
+    { name : "Corn", price : 300},
+    { name : "Chicken", price : 400}
+];
+
+let cashInRegister = 1000;
+const orderQueue = [];
+let nextOrderId = 1;
+
+/**
+ * addNewPizza()
+ * Utility method that takes a pizza object and adds to menu 
+ * */
+
+function addNewPizza(pizzaObject){
+    menu.push(pizzaObject);
+}
+
+/**
+ * placeOrder()
+ * Utility method that takes a pizza name as parameter and
+ * 1. finds that pizza object in the menu
+ * 2. adds the income(price) to the cashInRegister
+ * 3. pushes new order object to the orderQueue
+ *      {pizza : selectedPizzaObjFromStep1, status : "ordered"}
+ * 4. returns the new order object  
+ */
+
+function placeOrder(pizzaName){
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName);
+    if(!selectedPizza){
+        console.error(`${pizzaName}, does not exist in the menu`);
+        return;
+    }
+    cashInRegister += selectedPizza.price;
+    const newOrder = { orderId : nextOrderId++, pizza : selectedPizza, status : "Ordered"};
+    orderQueue.push(newOrder);
+    return newOrder;
+}
+
+/**
+ * completeOrder()
+ * Utility method that takes orderID as a parameter
+ * 1. finds the correct order in the queue and mark its status as "Completed"
+ * 2. return the found order from the function
+ */
+
+function completeOrder(orderId){
+    const order = menu.find(orderObj => orderObj.orderId == orderId);
+    order.status = "Completed";
+    return order;
+}
+
+addNewPizza({ name : "Tandori Chicken", price : 450});
+addNewPizza({ name : "Spicy Chicken", price : 400});
+console.log(menu);
+placeOrder("Tandori Chicken");
+console.log(orderQueue);
+completeOrder(1);
+console.log(cashInRegister);
+console.log(orderQueue);
